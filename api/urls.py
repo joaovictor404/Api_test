aa4817189ffc98e50d2c55a292b5a11eec7ac6d2
João Victor <108677091+joaovictor404@users.noreply.github.com>
2023-08-07 12:55:35 -0300

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import mostrar_dados_api
 
 urlpatterns = [
-    path('mostrar_api/', mostrar_dados_api, name='mostrar_dados_api'),
+    path('', mostrar_dados_api, name='mostrar_dados_api'),
 ]
